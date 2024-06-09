517073a22216f69546dd24d9a90807a5efb10292
lataria123 <latariatazo@gmail.com>
2024-06-09 21:52:13 +0400

    second commit

---
+++ level 1/homewok/lesson1.py
@@ -0,0 +1,15 @@
+name = "tazo"
+print(name)
+surname = "lataria"
+print(surname)
+print(name + " " + surname)
+
+print(name, surname)
+
+age = 10
+print(age)
+
+print(name + " " + surname + " " + str (age))
+
+height = 150
+print(name + " " + surname + " " + str (age) + " " + str (height))
